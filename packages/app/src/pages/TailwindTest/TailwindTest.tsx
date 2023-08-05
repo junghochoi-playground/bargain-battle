9cef0e57fb51849b98744837d86b793b300f53dd
dchoi315 <dchoi315@umd.edu>
2023-08-05 14:16:51 -0400

    Shadcn was installed

--- packages/app/src/pages/TailwindTest/TailwindTest.tsx
+++ packages/app/src/pages/TailwindTest/TailwindTest.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 const TailwindTest = () => {
 	return (
 		<div className="flex h-screen items-center justify-center bg-red-400">
-			Hello World
+			<Button>Hello World</Button>
 		</div>
 	)
 }
